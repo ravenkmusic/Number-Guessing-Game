7f856141e57980232095e0f8b7f1fe91e704f27f
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-02-07 15:19:56 +0000

    rearranged order of code so that actual game play can be stopped once user has run out of guesses

--- main.py
+++ main.py
@@ -12,10 +12,6 @@ import random
 easy_level = 10
 hard_level = 5
 
-print("Welcome to the Number Guessing Game!\nThe computer is thinking of a number between 1 and 100.")
-
-computer_choice = random.randint(1, 100)
-
 def difficulty():
   difficulty_level = input("Choose a difficulty level. Easy or hard? ")
   
@@ -26,30 +22,37 @@ def difficulty():
 
 def compare_choices(user_guess, computer_choice):
   if user_guess > computer_choice:
-    print(f"Too high. You have {turns} remaining. Enter another guess. ")
+    print(f"Too high. You have {turns} remaining. ")
   elif user_guess < computer_choice:
-      print(f"Too low. You have {turns} remaining. Enter another guess. ")
+      print(f"Too low. You have {turns} remaining. ")
     # If they got the answer correct, show the actual answer to the player.
   elif user_guess == computer_choice:
       print(f"The computer choice {computer_choice}.\nYou guessed {computer_choice}. You win!")
 
-turns = difficulty()
+def game():
+  print("Welcome to the Number Guessing Game!\nThe computer is thinking of a number between 1 and 100.")
 
-user_guess = 0
+  computer_choice = random.randint(1, 100)
 
-while user_guess != computer_choice:
-  user_guess = int(input("What number do you think it is? "))
-  turns -= 1
+  turns = difficulty()
+
+  user_guess = 0
+
+  while user_guess != computer_choice:
+    user_guess = int(input("What number do you think it is? "))
+    turns -= 1
+    
+    compare_choices(user_guess, computer_choice)
+  if turns == 0:
+    print("You've run out of guesses. You lose.")
+    return 
+
+  # Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+
   
-  compare_choices(user_guess, computer_choice)
+  # Track the number of turns remaining.
 
+  # If they run out of turns, provide feedback to the player.
 
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-
- 
-# Track the number of turns remaining.
-
-# If they run out of turns, provide feedback to the player.
-
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+  # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
