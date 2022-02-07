e6960c9975ba1d72ff47e868ea51f0a6559a88c3
Raven K <85319728+ravenkmusic@users.noreply.github.com>
2022-02-07 16:01:00 +0000

    removed all comments

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-#Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-
 from art import logo
 print(logo)
-
-# Allow the player to submit a guess for a number between 1 and 100.
 
 import random
 
@@ -25,13 +19,12 @@ def compare_choices(user_guess, computer_choice):
     print(f"You've guessed too high. ")
   elif user_guess < computer_choice:
       print(f"You've guessed too low. ")
-    # If they got the answer correct, show the actual answer to the player.
   elif user_guess == computer_choice:
       print(f"The computer choice {computer_choice}.\nYou guessed {computer_choice}. You win!")
 
 def game():
   print(f"Welcome to the Number Guessing Game!\nThe computer is thinking of a number between 1 and 100.")
-  
+
   computer_choice = random.randint(1, 100)
 
   turns = difficulty()
@@ -48,14 +41,5 @@ def game():
     if turns == 0:
       print("You've run out of guesses. You lose.")
       return 
-
-  # Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-
-  
-  # Track the number of turns remaining.
-
-  # If they run out of turns, provide feedback to the player.
-
-  # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
 game()
